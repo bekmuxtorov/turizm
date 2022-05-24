755c3819e961fb909628e9f9d919841333538c97
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2022-05-24 11:09:11 +0500

    updated

--- js/sendtelegram.js
+++ js/sendtelegram.js
@@ -1,18 +1,15 @@
 //bot token
 var telegram_bot_id = "5390439763:AAFjPSntY7IsmWloDrpKgoigEx2g4g_bfbA"; // token'ni o'rniga Siz yaratgan Bot tokenini yozing
 //chat id
-var chat_id = 1603330179; // 1111'ni o'rniga habar borishi kerak bo'lgan joyni ID'sini yozing (Batafsil videoda)
-var u_name, person_number, message, number;
-for(let j of li){
-    const manzil = j.querySelector("h2");
-    const y = manzil.textContent;
-}
+var chat_id =1603330179; // 1111'ni o'rniga habar borishi kerak bo'lgan joyni ID'sini yozing (Batafsil videoda)
+var u_name, person_number, message, number, qayerdan, qayerga;
 var ready = function() {
     u_name = document.getElementById("name").value;
     person_number = document.getElementById("person_number").value;
-    message = document.getElementById("message").value;
     number = document.getElementById("number").value;
-    message = "Ismi:  " + u_name + "\n\nTel raqami: " + number + "\n\nSayohatchilar soni: " + person_number +"\n\nYo'nalish:"+ y + "\n\nXabari: " + message;
+    qayerdan = document.getElementById("qayerdan").value;
+    qayerga = document.getElementById("qayerga").value;
+    message = "Ismi:  " + u_name + "\n\nTel raqami: " + number + "\n\nSayohatchilar soni: " + person_number +  "\n\nQayerdan: " + qayerdan +  "\n\nQayerga: " + qayerga;
     
 };
 var sendtelegram = function() {
@@ -36,7 +33,8 @@ var sendtelegram = function() {
     });
     document.getElementById("name").value = "";
     document.getElementById("person_number").value = "";
-    document.getElementById("message").value = "";
     document.getElementById("number").value = "";
+    document.getElementById("qayerdan").value = "";
+    document.getElementById("qayerga").value = "";
     return false;
 };
